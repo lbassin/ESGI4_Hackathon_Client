9056bd1f6e0cdc35bebb6faa5711b813c7dfcb45
Bassin Laurent <laurent@bassin.info>
2018-03-19 20:11:52 +0100

    Add SendRequest to API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {API_URL} from './app.vars';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  headers: {headers: HttpHeaders};
+
+  constructor(private http: HttpClient) {
+  }
+
+  public ngOnInit(): void {
+    this.headers = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': '1'
+      })
+    };
+  }
+
+  sendRequest(input: HTMLTextAreaElement) {
+    const data = {question: input.value};
+
+    this.http.post(API_URL, data, this.headers).toPromise().then(response => {
+      console.log(response);
+    });
+  }
 }

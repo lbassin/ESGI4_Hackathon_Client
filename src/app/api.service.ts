6964ee8075176a0edc71f112e810f50aff5bb9c0
Bassin Laurent <laurent@bassin.info>
2018-03-21 22:55:59 +0100

    Save init

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {API_URL} from './app.vars';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Router} from '@angular/router';
 import {MessageService} from './message-service';
@@ -11,13 +10,24 @@ export class ApiService {
   private session: string;
   private response: string;
 
+  private defaultUrl = 'http://127.0.0.1:3000/api/';
+  private initUrl = 'http://127.0.0.1:3000/init/';
+
+  private url = this.defaultUrl;
+
   constructor(private http: HttpClient,
               private router: Router,
               private messageService: MessageService) {
+    this.updateHeaders();
+  }
+
+  private updateHeaders() {
     let id = this.router.routerState.snapshot.url;
     if (id[0] === '/') {
       id = id.slice(1);
     }
+
+    console.log(id);
 
     this.headers = {
       headers: new HttpHeaders({
@@ -34,7 +44,8 @@ export class ApiService {
     };
 
     this.messageService.showQuestion(question);
-    this.http.post(API_URL, data, this.headers).toPromise()
+    console.log(this.headers);
+    this.http.post(this.url, data, this.headers).toPromise()
       .then((response: { type: string, data: any, session?: string }) => {
         this.response = JSON.stringify(response);
         this.session = response.session;
@@ -53,6 +64,12 @@ export class ApiService {
         break;
       case 'video':
         this.showVideo(response.data);
+        break;
+      case 'init':
+        this.showInit(response);
+        break;
+      case 'init_done':
+        this.doneInit(response);
         break;
     }
 
@@ -73,4 +90,28 @@ export class ApiService {
     this.messageService.showVideo(data.video);
   }
 
+  private showInit(data) {
+    if (data.session === 'genre') {
+      this.router.navigate([data.data.pseudo]);
+      setTimeout(() => {
+        this.messageService.showResponse(data.data.message);
+        this.updateHeaders();
+      }, 750);
+    }
+  }
+
+  private doneInit(data) {
+    this.setUrlToDefault();
+    setTimeout(() => {
+      this.messageService.showResponse(data.data.message);
+    }, 750);
+  }
+
+  setUrlToInit() {
+    this.url = this.initUrl;
+  }
+
+  setUrlToDefault() {
+    this.url = this.defaultUrl;
+  }
 }
--- src/app/bot.component.ts
+++ src/app/bot.component.ts
@@ -1,9 +1,35 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MessageService} from './message-service';
+import {ApiService} from './api.service';
 
 @Component({
   templateUrl: './bot.component.html',
   styleUrls: ['./bot.component.scss']
 })
-export class BotComponent {
+export class BotComponent implements OnInit {
+
+  private isKnownUser = false;
+
+  constructor(private messageService: MessageService, private apiService: ApiService) {
+  }
+
+  ngOnInit(): void {
+    if (!this.isKnownUser) {
+      setTimeout(() => {
+        this.startSetup();
+      }, 2500);
+    }
+  }
+
+  private startSetup() {
+    this.apiService.setUrlToInit();
+    this.messageService.showResponse('Bonjour et bienvenue sur Djingo ! :)');
+    this.messageService.showResponse('Avant de pouvoir utiliser nos services, nous avons besoin de quelques informations à votre sujet');
+
+    setTimeout(() => {
+      this.messageService.showResponse('Quel est votre prénom ?');
+    }, 1000);
+
+  }
 
 }
